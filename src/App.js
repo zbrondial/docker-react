ab63300e7a77e46588a13bc365eb6ac4f51a3c35
zbrondial <52339739+zbrondial@users.noreply.github.com>
2019-11-13 14:59:12 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yo There Zeus!
+          I was changed on the testdev branch!
         </p>
         <a
           className="App-link"
