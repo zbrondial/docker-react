ebaa3514938d8ab8e4c0ce21553a4c642fcff8fb
zbrondial <52339739+zbrondial@users.noreply.github.com>
2019-11-13 15:13:07 +0800

    Update app ps for testing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed on the testdev branch!
+          Testing CI/CD!
         </p>
         <a
           className="App-link"
